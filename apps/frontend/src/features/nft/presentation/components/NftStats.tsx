cd728f828620d77234789ad2cbdef5a498686187
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-10-28 13:18:35 +0000

    code review nft details

--- apps/frontend/src/features/nft/presentation/components/NftStats.tsx
+++ apps/frontend/src/features/nft/presentation/components/NftStats.tsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
-import S from '../../../../core/utilities/Main';
-import LaunchIcon from '@mui/icons-material/Launch';
 
-import SvgEthereum from '../../../../public/assets/vectors/ethereum-logo.svg';
-
-import '../styles/nft-view-history.css'
-import { ALIGN_CENTER, ALIGN_LEFT } from '../../../../core/presentation/components/TableDesktop';
 import TableState from '../../../../core/presentation/stores/TableState';
+import ViewNftPageStore from '../stores/ViewNftPageStore';
+
 import Table, { createTableCell, createTableCellString, createTableRow } from '../../../../core/presentation/components/Table';
+import { ALIGN_LEFT } from '../../../../core/presentation/components/TableDesktop';
 import TextWithTooltip from '../../../../core/presentation/components/TextWithTooltip';
 import Svg from '../../../../core/presentation/components/Svg';
 import NavRowTabs, { createNavRowTab } from '../../../../core/presentation/components/NavRowTabs';
 import ExtendedChart from '../../../../core/presentation/components/ExtendedChart';
-import ViewNftPageStore from '../stores/ViewNftPageStore';
+import AnimationContainer from '../../../../core/presentation/components/AnimationContainer';
 
-const PAGE_STATISTICS = 0;
-const PAGE_EARNINGS = 1;
-const PAGE_HISTORY = 2;
+import LaunchIcon from '@mui/icons-material/Launch';
+import SvgEthereum from '../../../../public/assets/vectors/ethereum-logo.svg';
+import '../styles/nft-stats.css'
+
+const TAB_STATISTICS = 0;
+const TAB_EARNINGS = 1;
+const TAB_HISTORY = 2;
 
 const EARNINGS_TABLE_LEGEND = ['', 'UTC Time', 'Local Time (-4:00 EDT)'];
 const EARNINGS_ROW_LEGEND = ['FPPS Calculation Period', 'Earnings Posting Time', 'Daily Payout Window', 'Minimum Daily Auto-withdraw'];
@@ -33,18 +34,65 @@ type Props = {
     viewNftPageStore: ViewNftPageStore;
 }
 
-export default function NftViewHistory({ viewNftPageStore }: Props) {
-    const [historyPage, setHistoryPage] = useState(PAGE_EARNINGS);
+export default function NftStats({ viewNftPageStore }: Props) {
+
+    const [selectedTab, setSelectedTab] = useState(TAB_EARNINGS);
+
+    function renderEarningsRows() {
+        const rows = [];
+
+        for (let i = 0; i < 4; i++) {
+            rows.push(
+                createTableRow([
+                    createTableCell((
+                        <TextWithTooltip text={EARNINGS_ROW_LEGEND[i]} tooltipText={EARNINGS_ROW_LEGEND_TOOLTIP[i]} />
+                    )),
+                    createTableCellString('00:00 - 23:59'),
+                    createTableCellString('20:00 - 19:59'),
+                ]),
+            )
+        }
+
+        return rows;
+    }
+
+    function renderHistoryRows() {
+        const rows = [];
+
+        for (let i = 0; i < 4; i++) {
+            rows.push(
+                createTableRow([
+                    createTableCellString(HISTORY_ROW_LEGEND[i]),
+                    createTableCell((
+                        <div className={'FlexRow GapCnt'}>
+                            <Svg svg={SvgEthereum} />
+                            1.65 ETH
+                        </div>
+                    )),
+                    createTableCellString('Harley'),
+                    createTableCellString('IDK'),
+                    createTableCell((
+                        <div className = { 'FlexRow GapCnt' }>
+                            3 months ago
+                            <Svg svg={LaunchIcon} />
+                        </div>
+                    )),
+                ]),
+            );
+        }
+
+        return rows;
+    }
 
     return (
-        <div className={'NftPreviewHistory FlexColumn'}>
+        <div className={'NftStats FlexColumn'}>
             <NavRowTabs navTabs={[
-                createNavRowTab('Reward Statistics', historyPage === PAGE_STATISTICS, () => setHistoryPage(PAGE_STATISTICS)),
-                createNavRowTab('Earnings Info', historyPage === PAGE_EARNINGS, () => setHistoryPage(PAGE_EARNINGS)),
-                createNavRowTab('History', historyPage === PAGE_HISTORY, () => setHistoryPage(PAGE_HISTORY)),
+                createNavRowTab('Reward Statistics', selectedTab === TAB_STATISTICS, () => setSelectedTab(TAB_STATISTICS)),
+                createNavRowTab('Earnings Info', selectedTab === TAB_EARNINGS, () => setSelectedTab(TAB_EARNINGS)),
+                createNavRowTab('History', selectedTab === TAB_HISTORY, () => setSelectedTab(TAB_HISTORY)),
             ]} />
             <div className={'HistoryContainer FlexColumn'}>
-                {historyPage === PAGE_STATISTICS && (
+                <AnimationContainer active = { selectedTab === TAB_STATISTICS } >
                     <ExtendedChart
                         className = { 'RewardsHistoryChart' }
                         headerItems={
@@ -54,8 +102,8 @@ export default function NftViewHistory({ viewNftPageStore }: Props) {
                             </div>
                         }
                         extendedChartState={ viewNftPageStore.extendedChartState } />
-                )}
-                {historyPage === PAGE_EARNINGS && (
+                </AnimationContainer>
+                <AnimationContainer active = { selectedTab === TAB_EARNINGS } >
                     <>
                         <div className={'HistoryContainerHeader'}>
                             <div className={'H3 Bold'}>Earnings Info</div>
@@ -69,8 +117,8 @@ export default function NftViewHistory({ viewNftPageStore }: Props) {
                             rows={renderEarningsRows()}
                         />
                     </>
-                )}
-                {historyPage === PAGE_HISTORY && (
+                </AnimationContainer>
+                <AnimationContainer active = { selectedTab === TAB_HISTORY } >
                     <>
                         <div className={'HistoryContainerHeader'}>
                             <div className={'H3 Bold'}>History</div>
@@ -84,54 +132,8 @@ export default function NftViewHistory({ viewNftPageStore }: Props) {
                             rows={renderHistoryRows()}
                         />
                     </>
-                )}
+                </AnimationContainer>
             </div>
         </div>
     )
-}
-
-function renderEarningsRows() {
-    const rows = [];
-
-    for (let i = 0; i < 4; i++) {
-        rows.push(
-            createTableRow([
-                createTableCell((
-                    <TextWithTooltip text={EARNINGS_ROW_LEGEND[i]} tooltipText={EARNINGS_ROW_LEGEND_TOOLTIP[i]} />
-                )),
-                createTableCellString('00:00 - 23:59'),
-                createTableCellString('20:00 - 19:59'),
-            ]),
-        )
-    }
-
-    return rows;
-}
-
-function renderHistoryRows() {
-    const rows = [];
-
-    for (let i = 0; i < 4; i++) {
-        rows.push(
-            createTableRow([
-                createTableCellString(HISTORY_ROW_LEGEND[i]),
-                createTableCell((
-                    <div className={'FlexRow GapCnt'}>
-                        <Svg svg={SvgEthereum} />
-                        1.65 ETH
-                    </div>
-                )),
-                createTableCellString('Harley'),
-                createTableCellString('IDK'),
-                createTableCell((
-                    <div className = { 'FlexRow GapCnt' }>
-                        3 months ago
-                        <Svg svg={LaunchIcon} />
-                    </div>
-                )),
-            ]),
-        );
-    }
-
-    return rows;
 }
