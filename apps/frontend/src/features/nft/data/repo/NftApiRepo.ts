bd2f6af12bf3818f83ee7bd4212236a2fb91fc9b
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-10-31 16:06:30 +0000

    update api

--- apps/frontend/src/features/nft/data/repo/NftApiRepo.ts
+++ apps/frontend/src/features/nft/data/repo/NftApiRepo.ts
@@ -1,3 +1,4 @@
+import { CollectionStatus } from '../../../collection/entities/CollectionEntity';
 import NftEntity from '../../entities/NftEntity';
 import NftRepo from '../../presentation/repos/NftRepo';
 import NftFilterModel from '../../utilities/NftFilterModel';
@@ -11,31 +12,34 @@ export default class NftApiRepo implements NftRepo {
         this.nftApi = new NftApi();
     }
 
-    async fetchNftById(nftId: string): Promise < NftEntity > {
+    async fetchNftById(nftId: string, status: CollectionStatus = CollectionStatus.APPROVED): Promise < NftEntity > {
         const nftFilterModel = new NftFilterModel();
         nftFilterModel.from = 0;
         nftFilterModel.count = Number.MAX_SAFE_INTEGER;
         nftFilterModel.nftIds = [nftId];
+        nftFilterModel.collectionStatus = status;
 
         const { nftEntities, total } = await this.fetchNftsByFilter(nftFilterModel);
         return nftEntities.length === 1 ? nftEntities[0] : null;
     }
 
-    async fetchNewNftDrops(): Promise < NftEntity[] > {
+    async fetchNewNftDrops(status: CollectionStatus = CollectionStatus.APPROVED): Promise < NftEntity[] > {
         const nftFilterModel = new NftFilterModel();
         // TO DO: sort by newest
         nftFilterModel.from = 0;
         nftFilterModel.count = Number.MAX_SAFE_INTEGER;
+        nftFilterModel.collectionStatus = status;
 
         const { nftEntities, total } = await this.fetchNftsByFilter(nftFilterModel);
         return nftEntities;
     }
 
-    async fetchTrendingNfts(): Promise < NftEntity[] > {
+    async fetchTrendingNfts(status: CollectionStatus = CollectionStatus.APPROVED): Promise < NftEntity[] > {
         const nftFilterModel = new NftFilterModel();
         // TO DO: sort by trending
         nftFilterModel.from = 0;
         nftFilterModel.count = Number.MAX_SAFE_INTEGER;
+        nftFilterModel.collectionStatus = status;
 
         const { nftEntities, total } = await this.fetchNftsByFilter(nftFilterModel);
         return nftEntities;
