5addfcafc733dce3621343b3f06324421cd01752
Stanislav Filipov <stanfilip0v@gmail.com>
2022-11-11 15:17:28 +0200

    migration for photo columns

--- apps/backend/database/migrations/20221110131717-add-image-fields.js
+++ apps/backend/database/migrations/20221110131717-add-image-fields.js
@@ -5,22 +5,22 @@ module.exports = {
        * Alter farms table - add columns main_image, banner_image
    */
         await queryInterface.addColumn('farms', 'images', {
-            type: Sequelize.ARRAY(Sequelize.TEXT),
+            type: Sequelize.ARRAY(Sequelize.BLOB),
             allowNull: true,
         })
 
         await queryInterface.addColumn('collections', 'main_image', {
-            type: Sequelize.TEXT,
+            type: Sequelize.BLOB,
             allowNull: true,
         })
 
         await queryInterface.addColumn('collections', 'banner_image', {
-            type: Sequelize.TEXT,
+            type: Sequelize.BLOB,
             allowNull: true,
         })
 
-        await queryInterface.changeColumn('nfts', 'uri', {
-            type: Sequelize.TEXT,
+        await queryInterface.addColumn('nfts', 'uri', {
+            type: Sequelize.BLOB,
             allowNull: true,
         });
     },
@@ -32,12 +32,6 @@ module.exports = {
         await queryInterface.removeColumn('farms', 'images')
         await queryInterface.removeColumn('collections', 'main_image')
         await queryInterface.removeColumn('collections', 'banner_image')
-        await queryInterface.changeColumn('nfts', 'uri', {
-            type: Sequelize.STRING,
-            allowNull: true,
-            validate: {
-                isUrl: true,
-            },
-        });
+        await queryInterface.removeColumn('nfts', 'uri')
     },
 };
