eb7bee90e24a4cd2d175af054a179db5a96e388b
VStaykov <v.staykov@razorlabs.com>
2022-11-21 10:03:10 +0000

    added info to admin dev seeder

--- apps/backend/database/dev-seeds/20221107080432-manufacturers_energy_sources_miners.js
+++ apps/backend/database/dev-seeds/20221107080432-manufacturers_energy_sources_miners.js
@@ -37,6 +37,7 @@ const users = [
     {
         email: 'superadmin@gmail.com',
         salt: '41vWHvhcqEeaY03TPVliL+EcpM4w+jbWd4OavgxmvBMQpXpch/CkYxUzaIJjm58rw0qKD42dFCn0RQdZcvJpyc/vObYOGaR5jXyQc4rfahWaKHM2BklsDUMxtMfWOwaToKed1jV4vQpbBFLnZ8bDC80m6iQlY+OaxDdwt2jp4ag=',
+        // pass is 123123
         hashed_pass: '90b7ade57ab677c332c86772a3066b53a1bba43ffa04c334cd0ae0244dc838075d5379ccbd0e37584865023cf719002c0e491092599ebad8a5948a7c204b2c6c',
         role: 'super_admin',
         cudos_address: 'wef',
