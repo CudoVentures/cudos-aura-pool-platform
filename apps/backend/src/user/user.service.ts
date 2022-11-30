ad3b0d50af837a750e24a8d1afc70fcaee325eab
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-11-30 13:17:46 +0000

    users cleanup

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,10 +1,7 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import nodemailer from 'nodemailer';
-import * as crypto from 'crypto';
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.model';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { EmailInterface } from './interfaces/email.interface';
 import { jwtConstants } from '../auth/constants';
@@ -16,28 +13,6 @@ export class UserService {
     private userModel: typeof User,
     private jwtService: JwtService,
     ) {}
-
-    generateRandomPassword(length: number) {
-        let password = '';
-        const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        const array = new Uint32Array(length);
-        crypto.webcrypto.getRandomValues(array);
-
-        for (let i = 0; i < length; i++) {
-            password += chars[array[i] % chars.length];
-        }
-
-        return password;
-    }
-
-    generateSalt() {
-        return crypto.randomBytes(128).toString('base64');
-    }
-
-    generateHashedPass(password: string, salt: string) {
-        return crypto.createHmac('sha512', salt).update(password).digest('hex');
-    }
 
     async sendEmail(email: EmailInterface): Promise <void> {
         const transporter = nodemailer.createTransport({
@@ -75,77 +50,6 @@ export class UserService {
         };
 
         this.sendEmail(verificationEmail)
-    }
-
-    async createFarmAdmin(createUserDto: CreateUserDto): Promise<User> {
-        const { password } = createUserDto
-        const salt = this.generateSalt();
-        const hashed_pass = this.generateHashedPass(password, salt);
-
-        const user = {
-            ...createUserDto,
-            hashed_pass,
-            salt,
-            role: 'farm_admin',
-            is_active: true,
-            email_verified: false,
-        };
-
-        const farmAdmin = await User.create(user);
-
-        this.sendVerificationEmail(farmAdmin);
-
-        return farmAdmin;
-    }
-
-    async findById(userId: number): Promise < User > {
-        return this.userModel.findByPk(userId);
-    }
-
-    async findOne(email: string): Promise<User> {
-        const user = await this.userModel.findOne({
-            where: {
-                email,
-            },
-        });
-
-        return user;
-    }
-
-    async updateOne(
-        id: number,
-        updateUserDto: Partial<UpdateUserDto>,
-    ): Promise<User> {
-        const [count, [user]] = await this.userModel.update(
-            { ...updateUserDto },
-            { where: { id }, returning: true },
-        );
-
-        return user;
-    }
-
-    async changePassword(id: number, oldPassword: string, password: string) {
-        const userToCheck = await this.userModel.findByPk(id);
-
-        if (!userToCheck) {
-            throw new NotFoundException('Incorrect id');
-        }
-
-        const hashedPass = this.generateHashedPass(oldPassword, userToCheck.salt);
-
-        if (userToCheck.hashed_pass !== hashedPass) {
-            throw new UnauthorizedException('Incorrect password');
-        }
-
-        const salt = this.generateSalt();
-        const hashed_pass = this.generateHashedPass(password, salt);
-
-        const [count, [user]] = await this.userModel.update(
-            { salt, hashed_pass },
-            { where: { id }, returning: true },
-        );
-
-        return user;
     }
 
     async verifyEmail(token: string): Promise<{ verified: boolean}> {
