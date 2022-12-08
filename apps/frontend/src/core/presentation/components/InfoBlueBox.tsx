736a891b62a31c5f48f7bc855e285316c7f9da76
Stanislav Filipov <stanfilip0v@gmail.com>
2022-12-08 14:57:28 +0200

    fix import

--- apps/frontend/src/core/presentation/components/InfoBlueBox.tsx
+++ apps/frontend/src/core/presentation/components/InfoBlueBox.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import '../styles/info-gray-box.css';
+import '../styles/info-blue-box.css';
 import Svg from './Svg';
 
 type Props = {
