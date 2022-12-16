63e2cc0697ca1a510e791839f1b4f0a9b156d964
Kamen Stoykov <k.stoykov@razorlabs.com>
2022-12-16 13:36:28 +0000

    account settings UI

--- apps/frontend/src/features/accounts/presentation/pages/CreditAccountSettings.tsx
+++ apps/frontend/src/features/accounts/presentation/pages/CreditAccountSettings.tsx
@@ -13,7 +13,7 @@ import PageAdminHeader from '../../../header/presentation/components/PageAdminHe
 import Input from '../../../../core/presentation/components/Input';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import Actions, { ActionsLayout } from '../../../../core/presentation/components/Actions';
-import Button, { ButtonType } from '../../../../core/presentation/components/Button';
+import Button, { ButtonColor, ButtonType } from '../../../../core/presentation/components/Button';
 import Svg from '../../../../core/presentation/components/Svg';
 import AnimationContainer from '../../../../core/presentation/components/AnimationContainer';
 import StyledContainer, { ContainerBackground, ContainerPadding } from '../../../../core/presentation/components/StyledContainer';
@@ -121,142 +121,143 @@ function CreditAccountSettings({ accountSessionStore, alertStore }: Props) {
     }
 
     const accountEntity = accountSessionStore.accountEntity;
-    const adminEntity = accountSessionStore.adminEntity;
+    // const adminEntity = accountSessionStore.adminEntity;
 
     return (
         <PageLayoutComponent className = { 'CreditAccountSettings' }>
 
             <PageAdminHeader />
 
-            <div className = { 'PageContent AppContent FlexColumn' } >
-                <div className={'Header FlexRow'}>
-                    <div className={'H2 Bold'}>Account Details</div>
-                </div>
-                <div className={'SettingsItem'}>
-                    <div className={'SettingsName'}>Account Email</div>
-                    <div className={'SettingsOptions'}>
-                        <div className = { 'SettingsDisplay FlexRow' } >
-                            <Input
-                                gray = { true }
-                                value={accountEntity?.email} />
-                            <Actions>
-                                <Button onClick={onClickEditEmail} disabled = { editEmail } >
-                                    <Svg svg={BorderColorIcon} />
-                                    Change
-                                </Button>
-                            </Actions>
+            <div className = { 'PageContent PageContentDefaultPadding AppContent' } >
+                <StyledContainer>
+                    <div className = { 'H2 ExtraBold CreditAccountSettingsBlockTitle' } >Account Settings</div>
+                    <div className = { 'CreditAccountSettingsBlockSubtitle' } >Manage your account details</div>
+                    <div className={'SettingsItem'}>
+                        <div className={'SettingsName'}>Account Owner</div>
+                        <div className={'SettingsOptions'}>
+                            <div className = { 'SettingsDisplay FlexRow' } >
+                                <Input
+                                    gray = { true }
+                                    value={accountEntity.name} />
+                                <Actions>
+                                    <Button onClick={onClickEditOwner} color = { ButtonColor.SCHEME_4 } disabled = { editOwner }>
+                                        <Svg svg={BorderColorIcon} />
+                                        Change
+                                    </Button>
+                                </Actions>
+                            </div>
+                            <AnimationContainer className = { 'SettingsEdit' } active = { editOwner } >
+                                <Input
+                                    label = { 'Enter new account name' }
+                                    inputValidation = { accountNameValidation }
+                                    value = { self.accountEntity.name }
+                                    onChange = { onChangeOwner } />
+                                <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
+                                    <Button type = { ButtonType.TEXT_INLINE } onClick = { onClickCancelEditOwner } >Discard Changes</Button>
+                                    <Button onClick = { onClickChangeOwner }>Save Changes</Button>
+                                </Actions>
+                            </AnimationContainer>
                         </div>
-                        <AnimationContainer className = { 'SettingsEdit' } active = { editEmail } >
-                            { changedEmail === false ? (
-                                <>
-                                    <Input
-                                        label = { 'Enter new email address' }
-                                        inputValidation = { accountEmailValidation }
-                                        value = { self.accountEntity?.email }
-                                        onChange = { onChangeEmail } />
-                                    <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
-                                        <Button type = { ButtonType.TEXT_INLINE } onClick = { onClickCancelEditEmail } >Discard Changes</Button>
-                                        <Button onClick = { onClickChangeEmail }>Save Changes</Button>
-                                    </Actions>
-                                </>
-                            ) : (
-                                <>
-                                    <StyledContainer containerPadding = { ContainerPadding.PADDING_16 } containerBackground = { ContainerBackground.GRAY } >
+                    </div>
+                    <div className={'SettingsItem'}>
+                        <div className={'SettingsName'}>Email</div>
+                        <div className={'SettingsOptions'}>
+                            <div className = { 'SettingsDisplay FlexRow' } >
+                                <Input
+                                    gray = { true }
+                                    value={accountEntity?.email} />
+                                <Actions>
+                                    <Button onClick={onClickEditEmail} color = { ButtonColor.SCHEME_4 } disabled = { editEmail } >
+                                        <Svg svg={BorderColorIcon} />
+                                    Change
+                                    </Button>
+                                </Actions>
+                            </div>
+                            <AnimationContainer className = { 'SettingsEdit' } active = { editEmail } >
+                                { changedEmail === false ? (
+                                    <>
+                                        <Input
+                                            label = { 'Enter new email address' }
+                                            inputValidation = { accountEmailValidation }
+                                            value = { self.accountEntity?.email }
+                                            onChange = { onChangeEmail } />
+                                        <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
+                                            <Button type = { ButtonType.TEXT_INLINE } onClick = { onClickCancelEditEmail } >Discard Changes</Button>
+                                            <Button onClick = { onClickChangeEmail }>Save Changes</Button>
+                                        </Actions>
+                                    </>
+                                ) : (
+                                    <>
+                                        <StyledContainer containerPadding = { ContainerPadding.PADDING_16 } containerBackground = { ContainerBackground.GRAY } >
                                         An email has been sent to you with a link to<br />
                                         verify your account.<br />
                                         Resend email if it doesn’t arrive in a minute.
-                                    </StyledContainer>
-                                    <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
-                                        <Button onClick = { onClickResendVerificationEmail }>Resend</Button>
-                                    </Actions>
-                                </>
-                            ) }
+                                        </StyledContainer>
+                                        <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
+                                            <Button onClick = { onClickResendVerificationEmail }>Resend</Button>
+                                        </Actions>
+                                    </>
+                                ) }
 
-                        </AnimationContainer>
+                            </AnimationContainer>
+                        </div>
                     </div>
-                </div>
-                <div className={'SettingsItem'}>
-                    <div className={'SettingsName'}>Account Owner</div>
-                    <div className={'SettingsOptions'}>
-                        <div className = { 'SettingsDisplay FlexRow' } >
-                            <Input
-                                gray = { true }
-                                value={accountEntity.name} />
-                            <Actions>
-                                <Button onClick={onClickEditOwner} disabled = { editOwner }>
-                                    <Svg svg={BorderColorIcon} />
+                    <div className={'SettingsItem'}>
+                        <div className={'SettingsName'}>Password</div>
+                        <div className={'SettingsOptions'}>
+                            <div className = { 'SettingsDisplay FlexRow' } >
+                                <Input
+                                    gray = { true }
+                                    value={'*****************'} />
+                                <Actions>
+                                    <Button onClick = { onClickEditPass } color = { ButtonColor.SCHEME_4 } disabled = { editPass }>
+                                        <Svg svg={BorderColorIcon} />
                                     Change
-                                </Button>
-                            </Actions>
-                        </div>
-                        <AnimationContainer className = { 'SettingsEdit' } active = { editOwner } >
-                            <Input
-                                label = { 'Enter new account name' }
-                                inputValidation = { accountNameValidation }
-                                value = { self.accountEntity.name }
-                                onChange = { onChangeOwner } />
-                            <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
-                                <Button type = { ButtonType.TEXT_INLINE } onClick = { onClickCancelEditOwner } >Discard Changes</Button>
-                                <Button onClick = { onClickChangeOwner }>Save Changes</Button>
-                            </Actions>
-                        </AnimationContainer>
-                    </div>
-                </div>
-                <div className={'SettingsItem'}>
-                    <div className={'SettingsName'}>Password</div>
-                    <div className={'SettingsOptions'}>
-                        <div className = { 'SettingsDisplay FlexRow' } >
-                            <Input
-                                gray = { true }
-                                value={'*****************'} />
-                            <Actions>
-                                <Button onClick = { onClickEditPass } disabled = { editPass }>
-                                    <Svg svg={BorderColorIcon} />
-                                    Change
-                                </Button>
-                            </Actions>
-                        </div>
-                        <AnimationContainer className = { 'SettingsEdit' } active = { editPass } >
-                            <Input
-                                label = { 'Current Password' }
-                                value = { oldPass }
-                                type = {'password'}
-                                onChange = { setOldPass } />
-                            <Input
-                                label = { 'New Password' }
-                                value = { pass }
-                                type = {'password'}
-                                inputValidation={[
-                                    validationPass,
-                                    validationFirstMatchPass,
-                                ]}
-                                onChange = { setPass } />
-                            <Input
-                                label = { 'Confirm Password' }
-                                value = { confirmPass }
-                                type = {'password'}
-                                inputValidation={[
-                                    validationConfirmPass,
-                                    validationSecondMatchPass,
-                                ]}
-                                onChange = { setConfirmPass } />
-                            <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
-                                <Button type = { ButtonType.TEXT_INLINE } onClick = { onClickCancelEditPass } >Discard Changes</Button>
-                                <Button onClick = { onClickChangePass } >Save Changes</Button>
-                            </Actions>
-                        </AnimationContainer>
-                    </div>
-                </div>
-                <div className={'SettingsItem'}>
-                    <div className={'SettingsName'}>Connected Wallet Address</div>
-                    <div className={'SettingsOptions'}>
-                        <div className = { 'SettingsDisplay FlexRow' } >
-                            <Input
-                                gray = { true }
-                                value={adminEntity.cudosWalletAddress} />
+                                    </Button>
+                                </Actions>
+                            </div>
+                            <AnimationContainer className = { 'SettingsEdit' } active = { editPass } >
+                                <Input
+                                    label = { 'Current Password' }
+                                    value = { oldPass }
+                                    type = {'password'}
+                                    onChange = { setOldPass } />
+                                <Input
+                                    label = { 'New Password' }
+                                    value = { pass }
+                                    type = {'password'}
+                                    inputValidation={[
+                                        validationPass,
+                                        validationFirstMatchPass,
+                                    ]}
+                                    onChange = { setPass } />
+                                <Input
+                                    label = { 'Confirm Password' }
+                                    value = { confirmPass }
+                                    type = {'password'}
+                                    inputValidation={[
+                                        validationConfirmPass,
+                                        validationSecondMatchPass,
+                                    ]}
+                                    onChange = { setConfirmPass } />
+                                <Actions layout = { ActionsLayout.LAYOUT_ROW_RIGHT } >
+                                    <Button type = { ButtonType.TEXT_INLINE } onClick = { onClickCancelEditPass } >Discard Changes</Button>
+                                    <Button onClick = { onClickChangePass } >Save Changes</Button>
+                                </Actions>
+                            </AnimationContainer>
                         </div>
                     </div>
-                </div>
+                    {/* <div className={'SettingsItem'}>
+                        <div className={'SettingsName'}>Connected Wallet Address</div>
+                        <div className={'SettingsOptions'}>
+                            <div className = { 'SettingsDisplay FlexRow' } >
+                                <Input
+                                    gray = { true }
+                                    value={adminEntity.cudosWalletAddress} />
+                            </div>
+                        </div>
+                    </div> */}
+                </StyledContainer>
             </div>
 
             <PageFooter />
