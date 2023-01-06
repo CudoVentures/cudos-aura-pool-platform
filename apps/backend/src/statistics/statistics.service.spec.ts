38ff1ac1f609668cc5537dafa70bc6d0990ed7eb
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-01-06 13:54:03 +0000

    initial test db

--- apps/backend/src/statistics/statistics.service.spec.ts
+++ apps/backend/src/statistics/statistics.service.spec.ts
@@ -11,23 +11,74 @@ import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from '../auth/auth.types';
 import { Sequelize } from 'sequelize-typescript';
+import compose from 'docker-compose';
+import Path from 'path';
+import { Umzug, SequelizeStorage } from 'umzug';
+import { read } from 'fs';
 
 describe('StatisticsService', () => {
     let service: StatisticsService;
     let module: TestingModule;
 
-    beforeAll(async () => {
-        // const sequelize = new Sequelize({
-        //     dialect: 'sqlite',
-        //     storage: ':memory:',
-        // });
-        // sequelize.addModels([
-        //     NftOwnersPayoutHistoryRepo,
-        //     NftPayoutHistoryRepo,
-        // ]);
-        // sequelize.getQueryInterface();
+    jest.setTimeout(60000);
 
-        // await sequelize.sync();
+    beforeAll(async () => {
+        const sequelize = new Sequelize({
+            dialect: 'postgres',
+            host: 'host.docker.internal',
+            port: 15432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'aura_pool_test',
+            logging: false,
+        });
+        const umzug = new Umzug({
+            migrations: {
+                glob: Path.join(Path.join(process.cwd(), 'apps/backend/database/migrations/*.js')),
+                resolve: ({ name, path, context }) => {
+                    const migration = require(path);
+                    return {
+                        name,
+                        up: async () => migration.up(context, Sequelize),
+                        down: async () => migration.down(context, Sequelize),
+                    }
+                },
+            },
+            context: sequelize.getQueryInterface(),
+            storage: new SequelizeStorage({ sequelize }),
+            logger: undefined,
+        });
+
+        await compose.upAll({
+            cwd: Path.join(process.cwd(), 'docker/test'),
+        });
+        await new Promise < void >((resolve, reject) => {
+            const interval = setInterval(async () => {
+                try {
+                    const sequelize2 = new Sequelize({
+                        dialect: 'postgres',
+                        host: 'host.docker.internal',
+                        port: 15432,
+                        username: 'postgres',
+                        password: 'postgres',
+                        database: 'aura_pool_test',
+                        logging: false,
+                    });
+                    sequelize2.connectionManager.initPools();
+                    const conn = await sequelize2.connectionManager.getConnection({ type: 'read' });
+                    sequelize2.connectionManager.releaseConnection(conn);
+                    sequelize2.connectionManager.close();
+                    clearInterval(interval);
+                    resolve();
+                } catch (ex) {
+                }
+            }, 500);
+        });
+
+        await sequelize.dropAllSchemas({
+            logging: false,
+        });
+        await umzug.up();
 
         module = await Test.createTestingModule({
             imports: [
@@ -54,12 +105,12 @@ describe('StatisticsService', () => {
                 SequelizeModule.forRoot({
                     dialect: 'postgres',
                     host: 'host.docker.internal',
-                    port: 5432,
+                    port: 15432,
                     username: 'postgres',
                     password: 'postgres',
-                    database: 'aura_pool',
+                    database: 'aura_pool_test',
                     autoLoadModels: true,
-                    synchronize: true,
+                    synchronize: false,
                     logging: false,
                 }),
                 SequelizeModule.forFeature([
@@ -83,6 +134,10 @@ describe('StatisticsService', () => {
 
     afterAll(async () => {
         await module.close();
+
+        await compose.down({
+            cwd: Path.join(__dirname, '../../../../docker/test'),
+        });
     });
 
     it('should be defined', () => {
