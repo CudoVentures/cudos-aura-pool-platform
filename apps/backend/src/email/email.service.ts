afdd64f0a463a3dc8db9f6dabfbaa03d96974f12
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-01-19 14:47:21 +0000

    update email tempalte

--- apps/backend/src/email/email.service.ts
+++ apps/backend/src/email/email.service.ts
@@ -4,6 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import nodemailer from 'nodemailer';
 import AccountEntity from '../account/entities/account.entity';
 import JwtToken from '../auth/entities/jwt-token.entity';
+import { EmailTemplateEntity } from './entities/email-template.entity';
 
 @Injectable()
 export default class EmailService {
@@ -27,13 +28,15 @@ export default class EmailService {
         try {
             const jwtToken = JwtToken.newInstance(accountEntity);
             const verificationToken = this.jwtService.sign(JwtToken.toJson(jwtToken), JwtToken.getConfig('1d'));
+            const emailTemplateEntity = new EmailTemplateEntity(`Hello ${accountEntity.name}! Welcome to Aura Pool. Please verify your email.`, 'Verify', `${this.appPublicUrl}/api/v1/accounts/verifyEmail/${verificationToken}`);
 
             const verificationEmail = {
                 from: 'noreply@aurapool.com',
                 to: accountEntity.email,
                 subject: 'Email Verification',
-                text: `Hello ${accountEntity.name}! Welcome to Aura Pool. Please verify your email by following this link ${this.appPublicUrl}/api/v1/accounts/verifyEmail/${verificationToken}`,
-                html: `<b>Hello ${accountEntity.name}! Welcome to Aura Pool. Please verify your email by following this link <a href="${this.appPublicUrl}/api/v1/accounts/verifyEmail/${verificationToken}">Verify</a></b>`,
+                // text: `Hello ${accountEntity.name}! Welcome to Aura Pool. Please verify your email by following this link ${this.appPublicUrl}/api/v1/accounts/verifyEmail/${verificationToken}`,
+                // html: `<b>Hello ${accountEntity.name}! Welcome to Aura Pool. Please verify your email by following this link <a href="${this.appPublicUrl}/api/v1/accounts/verifyEmail/${verificationToken}">Verify</a></b>`,
+                html: emailTemplateEntity.build(),
             };
 
             await this.transport.sendMail(verificationEmail);
@@ -46,13 +49,15 @@ export default class EmailService {
         try {
             const jwtToken = JwtToken.newInstance(accountEntity);
             const verificationToken = this.jwtService.sign(JwtToken.toJson(jwtToken), JwtToken.getConfig('1d'));
+            const emailTemplateEntity = new EmailTemplateEntity('You have requested a password change. Please follow the link below.', 'Reset password', `${this.appPublicUrl}/forgotten-pass-edit/${verificationToken}`);
 
             const verificationEmail = {
                 from: 'noreply@aurapool.com',
                 to: accountEntity.email,
                 subject: 'Forgotten password',
-                text: `You have requested a password change. Please follow this link ${this.appPublicUrl}/forgotten-pass-edit/${verificationToken}`,
-                html: `<b>You have requested a password change. Please follow this link <a href="${this.appPublicUrl}/forgotten-pass-edit/${verificationToken}">Change password</a></b>`,
+                // text: `You have requested a password change. Please follow this link ${this.appPublicUrl}/forgotten-pass-edit/${verificationToken}`,
+                // html: `<b>You have requested a password change. Please follow this link <a href="${this.appPublicUrl}/forgotten-pass-edit/${verificationToken}">Change password</a></b>`,
+                html: emailTemplateEntity.build(),
             };
 
             await this.transport.sendMail(verificationEmail);
