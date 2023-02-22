22ec7ad9400f2860a249e91f7c17eeccb7cea0d0
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-02-22 10:41:27 +0000

    allow list fetch

--- apps/frontend/src/allowlist/data/repo/AllowlistApiRepo.ts
+++ apps/frontend/src/allowlist/data/repo/AllowlistApiRepo.ts
@@ -5,25 +5,50 @@ import AllowlistApi from '../data-sources/AllowlistApi';
 export default class AllowlistApiRepo implements AllowlistRepo {
     allowlistApi: AllowlistApi;
 
+    enableActions: () => void;
+    disableActions: () => void;
+    showAlert: (msg: string, positiveListener?: null | (() => boolean | void), negativeListener?: null | (() => boolean | void)) => void;
+
     constructor() {
         this.allowlistApi = new AllowlistApi();
+
+        this.enableActions = null;
+        this.disableActions = null;
+        this.showAlert = null;
+    }
+
+    setPresentationActionsCallbacks(enableActions: () => void, disableActions: () => void) {
+        this.enableActions = enableActions;
+        this.disableActions = disableActions;
+    }
+
+    setPresentationAlertCallbacks(showAlert: (msg: string, positiveListener: null | (() => boolean | void), negativeListener: null | (() => boolean | void)) => void) {
+        this.showAlert = showAlert;
     }
 
     async fetchTotalListedUsers(): Promise < number > {
         try {
-            return this.allowlistApi.fetchTotalListedUsers();
+            this.disableActions?.();
+
+            return await this.allowlistApi.fetchTotalListedUsers();
         } catch (e) {
             console.log(e);
             return 0;
+        } finally {
+            this.enableActions?.();
         }
     }
 
     async fetchAllowlistUserBySessionAccount(): Promise < AllowlistUserEntity > {
         try {
-            return this.allowlistApi.fetchAllowlistUserBySessionAccount();
+            this.disableActions?.();
+
+            return await this.allowlistApi.fetchAllowlistUserBySessionAccount();
         } catch (e) {
             console.log(e);
             return null;
+        } finally {
+            this.enableActions?.();
         }
     }
 
--- apps/frontend/src/allowlist/presentation/repos/AllowlistRepo.ts
+++ apps/frontend/src/allowlist/presentation/repos/AllowlistRepo.ts
@@ -1,6 +1,9 @@
 import AllowlistUserEntity from '../../entities/AllowlistUserEntity';
 
 export default interface AllowlistRepo {
+    setPresentationActionsCallbacks(enableActions: () => void, disableActions: () => void);
+    setPresentationAlertCallbacks(showAlert: (msg: string, positiveListener : null | (() => boolean | void), negativeListener: null | (() => boolean | void)) => void);
+
     fetchAllowlistUserBySessionAccount(): Promise < AllowlistUserEntity >;
     fetchTotalListedUsers(): Promise < number >;
 }
