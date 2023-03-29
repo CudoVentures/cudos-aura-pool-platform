32de96e91458f084ed169acc41b1526a5341991d
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-03-29 11:39:01 +0000

    update auth middleware

--- apps/backend/src/auth/auth.middleware.ts
+++ apps/backend/src/auth/auth.middleware.ts
@@ -4,8 +4,10 @@ import { ExtractJwt } from 'passport-jwt';
 import { JwtService } from '@nestjs/jwt';
 import JwtToken from './entities/jwt-token.entity';
 import AccountService from '../account/account.service';
-import { AppRequest } from '../common/commont.types';
+import { RequestWithSessionAccounts } from '../common/commont.types';
 import { pbkdf2Sync } from 'node:crypto';
+import { Sequelize, Transaction } from 'sequelize';
+import { InjectConnection } from '@nestjs/sequelize';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
@@ -13,16 +15,20 @@ export class AuthMiddleware implements NestMiddleware {
     constructor(
         private jwtService: JwtService,
         private accountService: AccountService,
+        @InjectConnection()
+        private readonly sequelizeInstance: Sequelize,
     ) {}
 
-    async use(req: AppRequest, res: Response, next: NextFunction) {
+    async use(req: RequestWithSessionAccounts, res: Response, next: NextFunction) {
         const extractorFunction = ExtractJwt.fromAuthHeaderAsBearerToken();
 
         const encodedToken = extractorFunction(req);
+        const transaction: Transaction = await this.sequelizeInstance.transaction();
+
         try {
             this.jwtService.verify(encodedToken, JwtToken.getConfig());
             const jwtToken = JwtToken.fromJson(this.jwtService.decode(encodedToken));
-            const accounts = await this.accountService.findAccounts(jwtToken.id, req.transaction);
+            const accounts = await this.accountService.findAccounts(jwtToken.id, transaction);
             const derivedKey = pbkdf2Sync(accounts.accountEntity.hashedPass.substring(0, 10), 'salt', 100000, 64, 'sha512');
 
             if (derivedKey.toString('hex') !== jwtToken.derivedKey) {
@@ -33,11 +39,14 @@ export class AuthMiddleware implements NestMiddleware {
             req.sessionUserEntity = accounts.userEntity;
             req.sessionAdminEntity = accounts.adminEntity;
             req.sessionSuperAdminEntity = accounts.superAdminEntity;
+
+            await transaction.commit();
         } catch (ex) {
             req.sessionAccountEntity = null;
             req.sessionUserEntity = null;
             req.sessionAdminEntity = null;
             req.sessionSuperAdminEntity = null;
+            await transaction.rollback();
         }
 
         next();
