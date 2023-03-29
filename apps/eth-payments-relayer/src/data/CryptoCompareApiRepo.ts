83a7b1879ea56b780ca7c2a54c0ca518848f2cf7
VStaykov <v.staykov@razorlabs.com>
2023-03-29 12:42:21 +0000

    added env for crypto compare

--- apps/eth-payments-relayer/src/data/CryptoCompareApiRepo.ts
+++ apps/eth-payments-relayer/src/data/CryptoCompareApiRepo.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import BigNumber from 'bignumber.js';
+import config from '../../config/Config';
 import CryptoCompareRepo from '../workers/repos/CryptoCompareRepo';
 
 export default class CryptoCompareApiRepo implements CryptoCompareRepo {
@@ -8,13 +9,12 @@ export default class CryptoCompareApiRepo implements CryptoCompareRepo {
     static cryptoCompareCudosApiUrl = `${CryptoCompareApiRepo.cryptoCompareBaseUrl}?fsyms=CUDOS&tsyms=USD,ETH`
 
     async fetchEthUsdPrice(): Promise<number> {
-        const resultJson = await axios.get(CryptoCompareApiRepo.cryptoCompareEthApiUrl);
+        const resultJson = await axios.get(CryptoCompareApiRepo.cryptoCompareEthApiUrl, config.getCryptoCompareRequestHeader());
         return Number(resultJson.data.RAW.ETH.USD.PRICE)
     }
 
     async fetchCudosEthPrice(): Promise<BigNumber> {
-        const resultJson = await axios.get(CryptoCompareApiRepo.cryptoCompareCudosApiUrl);
+        const resultJson = await axios.get(CryptoCompareApiRepo.cryptoCompareCudosApiUrl, config.getCryptoCompareRequestHeader());
         return new BigNumber(resultJson.data.RAW.CUDOS.ETH.PRICE)
     }
-
 }
