a85b33ed21e0df57adf6b245ba9ba7e9a25921b3
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-04-04 12:27:46 +0000

    change collection name

--- apps/frontend/src/nft-presale/data/data-sources/CudoCollectionData.ts
+++ apps/frontend/src/nft-presale/data/data-sources/CudoCollectionData.ts
@@ -1,7 +1,7 @@
 import NftEntity, { NftGroup, NftTier, tierPriceMap } from '../../../nft/entities/NftEntity';
 
 export default {
-    name: 'Blockmole',
+    name: 'Blockmole 02',
     description: 'Borem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet. More',
     royalties: 3,
     totalNfts: 10000,
