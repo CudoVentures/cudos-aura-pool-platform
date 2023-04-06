f11d0d4944b4a4cb490268a16a272b989d5fdb15
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-04-06 11:55:20 +0000

    update cudo-collection-data

--- apps/frontend/src/nft-presale/data/data-sources/CudoCollectionData.ts
+++ apps/frontend/src/nft-presale/data/data-sources/CudoCollectionData.ts
@@ -1,7 +1,7 @@
 import NftEntity, { NftGroup, NftTier, tierPriceMap } from '../../../nft/entities/NftEntity';
 
 export default {
-    name: 'Bradd 01',
+    name: 'Bradd 02',
     description: 'The first collection from the renowed Bradd Styx and also the first collection for this platform. And some fantastic artwork attached. A bargain.',
     royalties: 3,
     totalNfts: 3333,
