75379828c8d5551da83a64f768d113fe7b1d420e
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-04-11 12:57:23 +0000

    add react key to NewLine component

--- apps/frontend/src/core/presentation/components/NewLine.tsx
+++ apps/frontend/src/core/presentation/components/NewLine.tsx
@@ -9,12 +9,12 @@ export default function NewLine({ text }: Props) {
         <>
             { text.split('\n').map((line: string, i: number, splitArray: string[]) => {
                 return (
-                    <>
+                    <span key = { i } >
                         { line }
                         { i + 1 < splitArray.length && (
                             <br />
                         ) }
-                    </>
+                    </span>
                 )
             }) }
         </>
